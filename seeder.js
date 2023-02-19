3ab0e4d56cf062580d717855f45ee73acefa22e0
Pema Ngodup <pngodup123@gmail.com>
2023-02-19 22:15:40 +0000

    seeder file is completed

--- seeder.js
+++ seeder.js
@@ -1,13 +1,24 @@
-const Note = require('./model/Note');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const fs = require('fs');
+
+const Note = require('./model/Note');
 
 // Configuring dotenv
 dotenv.config({ path: './config/config.env' });
 
 // Connecting to the Database
-mongoose.connect(process.env.MONGO_URI);
+async function connectDB() {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.9wgye.mongodb.net/${process.env.MONGO_DATABASE_NAME}`
+    );
+    console.log('Got connected');
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const notes = JSON.parse(
   fs.readFileSync(`${__dirname}/data/note.json`, 'utf-8')
@@ -15,6 +26,7 @@ const notes = JSON.parse(
 
 // import data
 const importData = async () => {
+  connectDB();
   try {
     await Note.create(notes);
     console.log('Data inserted');
@@ -26,16 +38,18 @@ const importData = async () => {
 
 // delete data
 const deleteData = async () => {
+  connectDB();
   try {
     await Note.deleteMany();
     console.log('Got deleted');
+    process.exit();
   } catch (error) {
     console.log(error);
   }
 };
 
-if (process.argv[1]) {
+if (process.argv[2] === '-i') {
   importData();
-} else if (process.argv[2]) {
+} else if (process.argv[2] === '-d') {
   deleteData();
 }
